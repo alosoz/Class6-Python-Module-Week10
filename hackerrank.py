828c1999f99a483cbb67ef99c94eaca5cd12c10c
unknown <alosbjk07@gmail.com>
2022-05-26 22:44:29 +0200

    Class6-Python-Module-Week10 - Shortcut

---
+++ hackerrank.py
@@ -0,0 +1,12 @@
+# Questin 1:
+
+# Select All SQL: https://www.hackerrank.com/challenges/select-all-sql/problem
+
+# select * from CITY; 
+
+
+# Question 2:
+
+# Weather Observation Station 1: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+# select CITY,STATE from STATION;
